9902c480a62e61d689f649f6cd9bd8ec27c39ca6
Nguyễn Tuấn Khải <nkhai462@gmail.com>
2022-03-13 09:22:44 +0700

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,9 +1,23 @@
-n = int(input('Number of Student in class '))
-
-for i in range(0,n):
-	N = input("Enter the fullname of student: ")
-	I = input("Enter the Student IDs: ")
-	M = input('Enter Marks: ')
-	C = input('Enter course: ')
-	C = (N, I, M)
-print(C)
+#storing information
+students = []
+student ={
+	"id": None,
+	"name": None,
+	"DoB": None,
+}
+#input function
+def addStudent():
+	student["id"] = input("Enter student id: ")
+	student["name"] = input("Enter student name: ")
+	student["DoB"] = input("Enter student DoB: ")
+	students.append(student)
+#list function
+def listStudent():
+    print("There %s student in the list" % len(students))
+    for i in students:
+		print("ID: %s, Name: %s, DoB: %s" % (i["id"], i["name"], i["DoB"]))
+#driver
+if __name__ == "__main__":
+    	addStudent()
+		listStudent()
+#end
