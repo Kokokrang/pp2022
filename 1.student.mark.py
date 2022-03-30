3845ac30589e421061e2cb23a2bb59dfb46c4834
Nguyễn Tuấn Khải <nkhai462@gmail.com>
2022-03-30 14:28:00 +0700

    Update 1.student.mark.py

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -19,9 +19,8 @@ def get_course_details():
     course_name = input("Enter course name: ")
     course_ID = input("Enter course ID: ")
     return course_name, course_ID
-#select a course, input marks for student in this course
-d = []
-def inputMarks():
+#input marks for student in this course
+def input_marks():
     while True:
         course_ID = input("Enter course ID: ")
         if course_ID not in [course[0] for course in course_list]:
@@ -29,11 +28,8 @@ def inputMarks():
             continue
         break
     marks = int(input("Enter marks: "))
-    if course_ID in d:
-        d[course_ID].append((student_ID, marks))
-    else:
-        d[course_ID] = [(student_ID, marks)]
-    print("Marks entered")
+    return course_ID, marks
+d = []
 #list all courses
 course_list = []
 def listCourses():
@@ -45,31 +41,28 @@ def listStudents():
     for student in student_list:
         print(f"Student ID: {student[0]} Student name: {student[1]} Student date of birth: {student[2]}")
 if __name__ == "__main__":
+    student_number = get_student_number()
+    for i in range(int(student_number)):
+        student_name, student_ID, student_DoB = get_student_details()
+        student_list.append((student_ID, student_name, student_DoB))
+    course_number = get_course_number()
+    for i in range(int(course_number)):
+        course_name, course_ID = get_course_details()
+        course_list.append((course_ID, course_name))
     while True:
-        print("\n1. Enter student details")
-        print("2. Enter course details")
-        print("3. Enter marks for student in course")
-        print("4. List all courses")
-        print("5. List all students")
-        print("6. Exit")
-        choice = int(input("Enter your choice: "))
-        if choice == 1:
-            student_name, student_ID, student_DoB = get_student_details()
-            student_list.append((student_ID, student_name, student_DoB))
-        elif choice == 2:
-            course_name, course_ID = get_course_details()
-            course_list.append((course_ID, course_name))
-        elif choice == 3:
-            inputMarks()
-        elif choice == 4:
+        print("\n1. Enter marks for student in course")
+        print("2. List all courses")
+        print("3. List all students")
+        print("4. Exit")
+        choice = input("Enter choice: ")
+        if choice == "1":
+            course_ID, marks = input_marks()
+            listMarks.append((course_ID, marks))
+        elif choice == "2":
             listCourses()
-        elif choice == 5:
-           student_list ()
-        elif choice == 6:
+        elif choice == "3":
+            listStudents()
+        elif choice == "4":
             break
         else:
             print("Invalid choice")
-    print(d)
-    print(student_list)
-    print(course_list)
-    print(listMarks)
